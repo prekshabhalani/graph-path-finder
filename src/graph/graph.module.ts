81b3015b792720044a8713f32b75094520218836
Preksha bhalani <59721431+prekshabhalani@users.noreply.github.com>
2025-06-11 02:17:51 +0530

    🚚 Rename resource and update import  for the standard structure

--- src/graph/graph.module.ts
+++ src/graph/graph.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphController } from './graph.controller';
 import { GraphService } from './graph.service';
-import { XmlParserService } from 'src/shared/xml-parser/xml-parser.service';
-import { PathfinderService } from 'src/shared/pathfinder/pathfinder.service';
+import { XmlParserService } from '../shared/xml-parser/xml-parser.service';
+import { PathfinderService } from '../shared/pathfinder/pathfinder.service';
 
 @Module({
   controllers: [GraphController],
