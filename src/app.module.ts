c1a4d5c662f4a24a49b50c5a66eb908c5718c19b
Preksha bhalani <59721431+prekshabhalani@users.noreply.github.com>
2025-06-10 15:07:16 +0530

    🔧 feat: add Swagger for API docs, Winston for logging, and Helmet for security
    - Integrated Swagger using @nestjs/swagger for automatic API documentation
    - Added Winston logger for structured and leveled logging
    - Applied Helmet middleware to enhance HTTP header security
    - Registered all middleware globally via `main.ts`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PathfinderModule } from './pathfinder/pathfinder.module';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
 
 @Module({
   imports: [ConfigModule.forRoot({
-      isGlobal: true,
-    }), PathfinderModule,],
+    isGlobal: true,
+  }),
+  WinstonModule.forRoot({
+    transports: [
+      new transports.Console({
+        format: format.combine(
+          format.timestamp(),
+          format.colorize(),
+          format.simple(),
+        ),
+      }),
+    ],
+  }), PathfinderModule,],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
+  //Winston for system logs
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  // Security middlewares
+  app.use(helmet());
+  app.enableCors();
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Graph Pathfinder API')
+    .setDescription('Find shortest path in a directed graph with all node details.')
+    .setVersion('1.0')
+    .addTag('pathfinder')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  //Dynamic URL log
+  const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
 
   const address = await app.getUrl();
-  console.log(`🚀 App is running at ${address}`);
+  app.get(WINSTON_MODULE_NEST_PROVIDER).log(`🚀 App is running at ${address}`);
 }
 bootstrap();
